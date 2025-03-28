6b0196552e8e9b4365a7cddb3419d23a8c9f90ad
Binbin0227 <1210850015@qq.com>
2025-03-28 15:22:00 +0800

    Update 迟到.py

--- 迟到.py
+++ 迟到.py
@@ -1,9 +1,9 @@
-a=eval(input("输入:"))
+a=eval(input("输入迟到次数:"))
 if a==0:
-    print("无")
+    print("处罚措施：无")
 elif a<=3:
-    print("书面")
+    print("处罚措施：书面")
 elif a<=5:
-    print("扣款")
+    print("处罚措施：扣款200元")
 elif a>5:
-    print("停职检查")
+    print("处罚措施：停职检查")
