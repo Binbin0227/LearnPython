8ac9e11b22d556427191da2d353b0837b020dc68
Binbin0227 <1210850015@qq.com>
2025-04-17 19:52:58 +0800

    Update 猜数字.py

--- 猜数字.py
+++ 猜数字.py
@@ -2,12 +2,12 @@ import random
 num = random.randint(0,9)
 n = 0
 while True:
-    guess = input("输入")
+    guess = input("请输入一个0 - 9的整数: ")
     n+=1
-    if guess == num:
-             ___________
-        print("猜中")
-    elif guess<num:
-              print(“遗憾太大了”)
-    elif guess>num:
-              print(“遗憾太小了”)
+    if int(guess) == num:
+        print(f"猜对了！你一共猜了{n}次")
+        break
+    elif int(guess) > num:
+        print("遗憾太大了")
+    elif int(guess) < num:
+        print("遗憾太小了")
